93d18539997c8f98d6452f4129723496119b4074
aravindhalahalli <aravind@byteally.com>
2023-09-23 03:03:35 +0530

    basic app in all

---
+++ React-Concept/custom-react-app-vite/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+import './index.css'
+
+function MyApp () {
+  return (
+    <div>
+      <h1>My app</h1>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <MyApp />
+  </React.StrictMode>,
+)
